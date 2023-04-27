6f4068b3ec67fdbbbd6b963020d45ff942cf2e6c
margarid4 <a45259@alunos.ipb.pt>
2023-04-27 21:08:24 +0100

    Enemies, Part 1: Static Observers
    Gargoyle

--- Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/GameEnding.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/GameEnding.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
@@ -8,7 +9,9 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
 
+    bool m_IsPlayerCaught;
     bool m_IsPlayerAtExit;
     float m_Timer;
 
@@ -16,29 +19,44 @@ public class GameEnding : MonoBehaviour
     {
         if (other.gameObject == player)
         {
-            bool m_IsPlayerAtExit = true ;
+            m_IsPlayerAtExit = true;
         }
     }
 
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
+
         }
     }
 
-    void EndLevel()
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
-
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
         if (m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
